2532368367fc0ab8e992263bd76fc37077b3197b
xuwanwan <1054749044@qq.com>
2017-10-01 13:19:08 +0800

    调试

--- js/song.js
+++ js/song.js
@@ -1,9 +1,27 @@
-var $disC = $('.disc-container')
+const $disC = $('.disc-container')
+let audio = document.createElement('audio')
+
+$.ajax({
+  url: './sources/song.json',
+  method: 'get'
+}).done(function(data){
+  console.log(data)
+})
+  .fail(function(){
+    alert('error')
+  })
+
+audio.oncanplay = function(){
+  audio.play()
+  $disC.addClass('playing')
+}
 
 $('.icon-play').on('click',function(){
+  audio.play()
   $disC.addClass('playing')
 })
 
 $('.icon-pause').on('click',function(){
+  audio.pause()
   $disC.removeClass('playing')
-})
+}) 
