4b8132c465ee479d49a1908cdabedb7ab9c0f26a
xuwanwan <1054749044@qq.com>
2017-10-03 21:27:13 +0800

    完成热歌榜和搜索的html,css

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 $(function(){
-  $.get('./sources/song.json').then(function(response){
+  $.get('./sources/index.json').then(function(response){
     let data = response
     let arr = data.map(s=>{
       let html = '<li><a href="./song.html?id=' + (data.indexOf(s) + 1) + '"' + '><h3>' + s.name + '</h3><p>' + (parseInt(s.sq) === 1?'<svg class="icon icon-sq" aria-hidden="true"><use xlink:href="#icon-sq"></use></svg>':'') + s.singer + '-' + s.name + '</p><svg class="icon icon-play" aria-hidden="true"><use xlink:href="#icon-play1"></use></svg></a></li>'
@@ -20,18 +20,34 @@ $(function(){
     let $li = $('.tabContent > li').eq(index)
     if($li.attr('data-download') === 'yes'){return}
     if(index === 1){
-      $.get('./sources/page2.json').then(response=>{
+      $.get('./sources/index2.json').then(response=>{
         $('.loading').eq(index).addClass('active')
         $li.attr('data-download','yes')
-        $li.append($(`<div>${response}<div>`))
+        HotData(response,index)
       })
     }else if(index === 2){
-      $.get('./sources/page3.json').then(response=>{
+      $.get('./sources/index3.json').then(response=>{
         $('.loading').eq(index).addClass('active')
         $li.attr('data-download','yes')
-        $li.append($(`<div>${response}<div>`))
+        searchData(response)
       })
     }
   })
+
+  function searchData(data){
+    let html = ``
+  }
+
+  function HotData(data,index){
+    let {hotCover, hotText, date, song} = data[0]
+    let html = `<img class="hotCover" src="${hotCover}" alt="热歌榜封面">
+      <img class="hotText" src="${hotText}" alt="热歌榜文字">
+      <p class="hotDate">${date}</p>`
+    song.map(s=>{
+      let songList = '<li><a href="./song.html?id=' + (song.indexOf(s) + 1) + '"' + '><h3>' + s.name + '</h3><p>' + (parseInt(s.sq) === 1?'<svg class="icon icon-sq" aria-hidden="true"><use xlink:href="#icon-sq"></use></svg>':'') + s.singer + '-' + s.name + '</p><svg class="icon icon-play" aria-hidden="true"><use xlink:href="#icon-play1"></use></svg></a></li>'
+      $('.tabContent > li > ol').append($(songList))
+    })
+    $('.intro').append($(html))
+  }
 
 })
